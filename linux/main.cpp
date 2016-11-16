f4f9d6b5397b8ffd10176686b9215792395f1353
Claes Ladefoged <claesnl@gmail.com>
2016-11-16 17:04:24 +0100

    Added age check fix

--- linux/main.cpp
+++ linux/main.cpp
@@ -217,6 +217,11 @@ int mni_register_brain_from_atlas(const char* dcmfolder){
   			string svalue(value);
   			string::size_type sz;
   			int age = stoi (svalue,&sz);
+  			if(svalue.compare(svalue.size()-1,1,"M") == 0){
+  				age = round(age/12);
+  			}
+  			
+  			return 0;
   			if (age <= 2){
   				cout << " - CHOSE ATLAS: 17-21w = 1.5-2y (should use more precise estimation of age!)" << endl;
   				atlasMNC="/usr/local/share/RESOLUTE/nihpd_asym_17-21_t1w.mnc";
